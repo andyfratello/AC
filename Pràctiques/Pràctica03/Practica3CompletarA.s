3a1c2904adcef09c51390b5bfb82720f4a2453ed
andyfratello <52325081+andyfratello@users.noreply.github.com>
2021-05-21 17:00:42 +0200

    Add files via upload

---
+++ Pràctiques/Pràctica03/Practica3CompletarA.s
@@ -0,0 +1,24 @@
+.text
+	.align 4
+	.globl OperaVec
+	.type	OperaVec, @function
+OperaVec:
+	pushl	%ebp
+	movl	%esp, %ebp
+	subl	$16, %esp
+	pushl	%ebx
+	pushl	%esi
+	pushl	%edi
+	movl	8(%ebp), %eax
+	movl	(%eax), %eax
+	movl	%eax, -4(%ebp)
+# Aqui has de introducir el codigo
+
+# El final de la rutina ya esta programado
+	movl	-4(%ebp), %eax
+	popl	%edi
+	popl	%esi
+	popl	%ebx
+	movl %ebp,%esp
+	popl %ebp
+	ret
---
+++ Pràctiques/Pràctica03/Practica3CompletarB.s
@@ -0,0 +1,21 @@
+.text
+	.align 4
+	.globl OperaMat
+	.type	OperaMat, @function
+OperaMat:
+	pushl	%ebp
+	movl	%esp, %ebp
+	subl	$16, %esp
+	pushl	%ebx
+	pushl	%esi
+	pushl	%edi
+# Aqui has de introducir el codigo
+
+# El final de la rutina ya esta programado
+	movl	-4(%ebp), %eax
+	popl	%edi
+	popl	%esi
+	popl	%ebx	
+	movl %ebp,%esp
+	popl %ebp
+	ret
