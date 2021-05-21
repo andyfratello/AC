c1a1397b1b61bb2ff93c52af3540760d088301e7
andyfratello <52325081+andyfratello@users.noreply.github.com>
2021-05-21 17:02:30 +0200

    Add files via upload

---
+++ Pràctiques/Pràctica06/MiSimulador.c
@@ -0,0 +1,68 @@
+#include "CacheSim.h"
+
+/* Posa aqui les teves estructures de dades globals
+ * per mantenir la informacio necesaria de la cache
+ * */
+
+
+
+
+/* La rutina init_cache es cridada pel programa principal per
+ * inicialitzar la cache.
+ * La cache es inicialitzada al començar cada un dels tests.
+ * */
+void init_cache ()
+{
+	/* Escriu aqui el teu codi */
+
+
+
+}
+
+/* La rutina reference es cridada per cada referencia a simular */ 
+void reference (unsigned int address, unsigned int LE)
+{
+	unsigned int byte;
+	unsigned int bloque_m; 
+	unsigned int linea_mc;
+	unsigned int tag;
+	unsigned int miss;
+	unsigned int lec_mp;
+	unsigned int mida_lec_mp;
+	unsigned int esc_mp;
+	unsigned int mida_esc_mp;
+	unsigned int replacement;
+	unsigned int tag_out;
+
+	/* Escriu aqui el teu codi */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/* La funcio test_and_print escriu el resultat de la teva simulacio
+	 * per pantalla (si s'escau) i comproba si hi ha algun error
+	 * per la referencia actual
+	 * */
+	test_and_print (address, LE, byte, bloque_m, linea_mc, tag,
+			miss, lec_mp, mida_lec_mp, esc_mp, mida_esc_mp,
+			replacement, tag_out);
+}
+
+/* La rutina final es cridada al final de la simulacio */ 
+void final ()
+{
+ 	/* Escriu aqui el teu codi */ 
+  
+  
+}
