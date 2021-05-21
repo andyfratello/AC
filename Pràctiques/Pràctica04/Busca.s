f6a0e4424ed0b6f960a8c15526d56e58ae93aa3f
andyfratello <52325081+andyfratello@users.noreply.github.com>
2021-05-21 17:01:26 +0200

    Add files via upload

---
+++ Pràctiques/Pràctica04/Busca.s
@@ -0,0 +1,6 @@
+ .text
+	.align 4
+	.globl Buscar
+	.type Buscar,@function
+Buscar:
+        # Aqui viene vuestro codigo
---
+++ Pràctiques/Pràctica04/BuscaElemento.s
@@ -0,0 +1,6 @@
+ .text
+	.align 4
+	.globl BuscarElemento
+	.type BuscarElemento,@function
+BuscarElemento:
+        # Aqui viene vuestro codigo
